69092df5b02bee3fdcbc8af1032c4408b45aa39b
sana1207 <sanafatema97@gmail.com>
2020-05-03 15:50:12 +0400

    First Commit

---
+++ app/src/main/java/com/example/movieapp/ui/single_movie_details/MovieDetailsRepository.kt
@@ -0,0 +1,27 @@
+package com.example.movieapp.ui.single_movie_details
+
+import androidx.lifecycle.LiveData
+import com.example.movieapp.data.api.TheMovieDBInterface
+import com.example.movieapp.data.repository.MovieDetailsNetworkDataSource
+import com.example.movieapp.data.repository.NetworkState
+import com.example.movieapp.data.vo.MovieDetails
+import io.reactivex.disposables.CompositeDisposable
+
+
+class MovieDetailsRepository (private val apiService : TheMovieDBInterface) {
+
+    lateinit var movieDetailsNetworkDataSource: MovieDetailsNetworkDataSource
+
+    fun fetchSingleMovieDetails(compositeDisposable: CompositeDisposable, movieId: Int): LiveData<MovieDetails> {
+
+        movieDetailsNetworkDataSource = MovieDetailsNetworkDataSource(apiService, compositeDisposable)
+        movieDetailsNetworkDataSource.fetchMovieDetails(movieId)
+
+        return movieDetailsNetworkDataSource.downloadedMovieResponse
+
+    }
+
+    fun getMovieDetailsNetworkState(): LiveData<NetworkState> {
+        return movieDetailsNetworkDataSource.networkState
+    }
+}
